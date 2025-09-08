39a85dbbff55065adcae0fcddf55a4918481425f
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-08 22:06:48 +0200

    Updated MongoDB connection options to remove deprecated option 'useFindAndModify'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Successfully connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
