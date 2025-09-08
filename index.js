eac8e1536a0a5bfb05095cd7087a4260789312fc
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-08 22:06:09 +0200

    Update console message for MongoDB connection

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Placeholder routes
 app.get('/', (req, res) => {
